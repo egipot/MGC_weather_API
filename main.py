6d6cae39d8777012299f908d08f4d55aa6d225f4
Gay Calaranan <gaycalaranan@gmail.com>
2024-11-25 14:29:15 +0200

    Day29 - 266

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+import pandas
 
-app = Flask('Website')
+app = Flask(__name__)
 
-@app.route('/home')
+
+@app.route('/')
 def home():
     return render_template('home.html')
 
@@ -18,8 +20,18 @@ def contact():
 def store():
     return render_template('store.html')
 
+@app.route('/api/v1/<station>/<date>')
+def home_api(station, date):
+    df = pandas.read_csv("")
+    temperature = df.station(date)
+    return {"station:" : station, 
+            "date:" : date, 
+            "temperature:" : temperature}
+    #render_template('home-api.html')
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
