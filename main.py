f1d7ee61f7412609c55f824955a4ae3394fc0aa3
Gay Calaranan <gaycalaranan@gmail.com>
2024-11-26 14:01:15 +0200

    Day31 completed

--- main.py
+++ main.py
@@ -31,13 +31,30 @@ def weather_api_query(station, date):
     temperature = df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze() / 10
     # print(station)
     # print(temperature)
-    
     #here, get the temperature value based on filtered date 
     #df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze() / 10
     
     return {"station:" : station, 
             "date:" : date, 
             "temperature:" : temperature}
+
+
+@app.route('/api/v1/<station>')
+def weather_api_all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pandas.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient='records')
+    return result
+
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def weather_api_all_year(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pandas.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return result
 
 
 
