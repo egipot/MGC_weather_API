ae5052b982f0a83240f3b3b275986a56db8bf1b0
Gay Calaranan <gaycalaranan@gmail.com>
2024-11-26 13:34:51 +0200

    Day 31 #278 Show data and and stations on the webpage

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import pandas
 
 app = Flask(__name__)
 
+stations = pandas.read_csv('data_small/stations.txt', skiprows=17, index_col=0)
+#print specific columns (station ID and station name), removing the rest of columns
+#stations = stations[['STAID', 'STANAME                                 ']]
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home-api.html', data=stations.to_html())
 
 @app.route('/about/')
 def about():
@@ -22,21 +25,20 @@ def store():
     return render_template('store.html')
 
 @app.route('/api/v1/<station>/<date>')
-def home_api(station, date):
+def weather_api_query(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pandas.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze() / 10
-    print(station)
-    print(temperature)
+    # print(station)
+    # print(temperature)
     
     #here, get the temperature value based on filtered date 
     #df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze() / 10
     
-    #temperature = df.station(date)
     return {"station:" : station, 
             "date:" : date, 
             "temperature:" : temperature}
-    #render_template('home-api.html')
+
 
 
 if __name__ == "__main__":
